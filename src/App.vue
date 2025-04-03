35eb0e1f5b9399fb0392d4d884421c363a897560
Mirkel1998 <mikped01@easv365.dk>
2025-04-03 21:31:28 +0200

    linking pages to buttons
    linked the different pages, so the button shows on the vue page ''navbar''

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <RouterLink to="/artworks">Artworks</RouterLink>
       </nav>
     </div>
   </header>
