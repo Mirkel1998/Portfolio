dec8c308cddb8c7e832bc349f6473323178be255
Mirkel1998 <mikped01@easv365.dk>
2025-04-24 09:48:30 +0200

    CAROUSEL IS FINALLY WORKING
    YAY

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,25 +1,33 @@
 <template>
 
-    <footer class="bg-black h-40 w-full flex">
-        <div class="footerContent1 w-1/3">
+
+        <footer class="bg-black h-40 w-full flex">
+        <div class="footerContent1 w-1/3 hidden sm:block">
             <h1 class="text-pinkyWhite font-bold pl-5 text-2xl pt-5">Mikkel Bech</h1>
             <p class="text-purpleRain pl-5 text-xs pt-3">All rights reserved 2025</p>
         </div>
-
-                <div class="w-1/3 text-center">
-            <router-link to="/" class="text-pinkyWhite block pt-5">Home</router-link>
+    
+        <div class="w-full text-center flex flex-col justify-center items-center sm:w-1/3 sm:block">
+            <router-link to="/" class="text-pinkyWhite block pt-2">Home</router-link>
             <router-link to="/about" class="text-pinkyWhite block pt-2">About</router-link>
             <router-link to="/projects" class="text-pinkyWhite block pt-2">Projects</router-link>
             <router-link to="/artworks" class="text-pinkyWhite block pt-2">Artworks</router-link>
         </div>
-
-        <div class="w-1/3 pt-10 pl-50">
+    
+        <div class="w-1/3 pt-10 pl-50 hidden sm:block">
             <p class="text-purpleRain">Check out my github!</p>
-                <a href="https://github.com/Mirkel1998" target="_blank" rel="noopener noreferrer">
-          <img class="" src="../components/images/githubLogo.svg" width="50" height="50" alt="GitHub Logo">
-        </a>
+            <a href="https://github.com/Mirkel1998" target="_blank" rel="noopener noreferrer">
+                <img class="" src="../components/images/githubLogo.svg" width="50" height="50" alt="GitHub Logo">
+            </a>
         </div>
- 
     </footer>
+
+    
+
+    
+
+    
+
+    
     
 </template>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -2,22 +2,33 @@
 <script setup>
 import FooterView from '../components/FooterView.vue'
 
+
 import { VueperSlides, VueperSlide } from 'vueperslides'
 import 'vueperslides/dist/vueperslides.css'
 
 import { ref } from 'vue'
 
+// import hver billede
+import mini_me from '../components/images/mini_me.png'
+
+
 const slides = ref([
   {
     title: 'Slide #1',
-    content: 'Slide 1 content.'
+    image: mini_me,
     
   },
   {
     title: 'Slide #2',
-    content: 'Slide 2 content.',
+    image: mini_me,
+  
     
-  }
+  },
+  {
+    title: 'Slide #1',
+    image: mini_me,
+    
+  },
 ])
 </script>
 
@@ -29,9 +40,12 @@ const slides = ref([
       v-for="(slide, i) in slides"
       :key="i"
 
-      
+    
       :title="slide.title"
-      :content="slide.content">
+
+         :image="slide.image"
+      >
+      <!-- <img :src="slide.image" alt="Slide image" style="width: 100%; height: auto;" />  -->
       
     </vueper-slide>
   </vueper-slides>
